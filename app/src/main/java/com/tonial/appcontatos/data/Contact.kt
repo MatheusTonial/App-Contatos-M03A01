8c6a58d15397ce2493e1ef1d19dd81cfb5090e93
Matheus <matheustonial97@gmail.com>
2026-03-14 11:33:40 -0300

    Add `Contact` data class and initial UI components for `ContactsListScreen`, including `AppBar` and `LoadingState` composables.

---
+++ app/src/main/java/com/tonial/appcontatos/data/Contact.kt
@@ -0,0 +1,11 @@
+package com.tonial.appcontatos.data
+
+data class Contact(
+    val id: Int = 0,
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
+    val isFavorite: Boolean = false
+)
+
