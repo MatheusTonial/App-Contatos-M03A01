3b477b3a9206d6f7676d6f02fbe00ec839344e42
Matheus <matheustonial97@gmail.com>
2026-03-14 13:08:56 -0300

    Add `ErrorState` and `EmptyList` composables with corresponding previews to `ContactsListScreen.kt`.

--- app/src/main/java/com/tonial/appcontatos/ui/contact/ContactsListScreen.kt
+++ app/src/main/java/com/tonial/appcontatos/ui/contact/ContactsListScreen.kt
@@ -1,13 +1,20 @@
 package com.tonial.appcontatos.ui.contact
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CloudOff
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,8 +22,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tonial.appcontatos.R
 import com.tonial.appcontatos.ui.theme.AppContatosTheme
 
 @Composable
@@ -71,5 +81,89 @@ fun LoadingState(modifier: Modifier = Modifier) {
 fun LoadingStatePreview() {
     AppContatosTheme() {
         LoadingState()
+    }
+}
+
+@Composable
+fun ErrorState(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = Icons.Filled.CloudOff,
+            contentDescription = "Erro ao carregar",
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(80.dp)
+        )
+        val textPadding = PaddingValues(
+            top = 8.dp,
+            start = 8.dp,
+            end = 8.dp
+        )
+        Text(
+            modifier = Modifier.padding(textPadding),
+            text = "Erro ao carregar contatos",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            modifier = Modifier.padding(textPadding),
+            text = "Aguarde um momento e tente novamente",
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.primary
+        )
+        ElevatedButton(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = {}
+        ) {
+            Text("Tentar novamente")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorStatePreview(modifier: Modifier = Modifier) {
+    AppContatosTheme {
+        ErrorState()
+    }
+}
+
+@Composable
+fun EmptyList(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(all = 16.dp).fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.no_data),
+            contentDescription = "Nenhum contato encontrado",
+            modifier = Modifier.size(250.dp)
+        )
+        Text(
+            text = "Nada por aqui...",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodyLarge,
+            color =  MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Você ainda não adicionou nenhum contato" +
+                    "\nAdicione um clicando no botão abaixo",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EmptyListPreview() {
+    AppContatosTheme {
+        EmptyList()
     }
 }
