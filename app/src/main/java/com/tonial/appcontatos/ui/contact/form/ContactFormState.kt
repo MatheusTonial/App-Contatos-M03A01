18f4318384d4a6763114a87443e41bbbac8103e4
Matheus <matheustonial97@gmail.com>
2026-03-28 11:13:14 -0300

    Implement scrollable form and introduce state management structures for `ContactFormScreen`.
    - Enable vertical scrolling in the `ContactFormScreen` main layout.
    - Add `FromField` and `FrormState` data classes to `ContactFormState.kt` to manage individual field values and validation messages.
    - Add `FormEvent` sealed class to handle UI events for updating contact information, including name, phone, email, and type.
    - Update `ContactFormState` to include the new `FrormState`.

--- app/src/main/java/com/tonial/appcontatos/ui/contact/form/ContactFormState.kt
+++ app/src/main/java/com/tonial/appcontatos/ui/contact/form/ContactFormState.kt
@@ -1,12 +1,43 @@
 package com.tonial.appcontatos.ui.contact.form
 
 import com.tonial.appcontatos.data.Contact
+import com.tonial.appcontatos.data.ContactTypeEnum
+import java.time.LocalDate
+
+data class FromField<T>(
+    val value: T,
+    val errorMessage: String = ""
+)
+
+data class FrormState(
+    val firstName: FromField<String> = FromField(""),
+    val lastName: FromField<String> = FromField(""),
+    val phoneNumber: FromField<String> = FromField(""),
+    val email: FromField<String> = FromField(""),
+    val isFavorite: FromField<Boolean> = FromField(false),
+    val assetValue: FromField<String> = FromField(""),
+    val birthDate: FromField<LocalDate> = FromField(LocalDate.now()),
+    val type: FromField<ContactTypeEnum> = FromField(ContactTypeEnum.PERSONAL)
+    )
 
 data class ContactFormState(
     val contactId: Int = 0,
     val isLoading: Boolean = false,
     val contact: Contact = Contact(),
     val hasErrorLoading: Boolean = false,
+    val formState: FrormState = FrormState()
 ){
     val isNewContatct get() : Boolean = contactId <= 0
+}
+
+sealed class FormEvent{
+    data class UpdateFirstName(val newValue: String) : FormEvent()
+    data class UpdateLastName(val newValue: String) : FormEvent()
+    data class UpdatePhoneNumber(val newValue: String) : FormEvent()
+    data class UpdateEmail(val newValue: String) : FormEvent()
+    data class UpdateIsFavorite(val newValue: Boolean) : FormEvent()
+    data class UpdateAssetValue(val newValue: String) : FormEvent()
+    data class UpdateBirthDate(val newValue: LocalDate) : FormEvent()
+    data class UpdateType(val newValue: ContactTypeEnum) : FormEvent()
+
 }
